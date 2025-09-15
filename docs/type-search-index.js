ee67b951a3e7a52def15cfdc128a998caad977d1
Taras / <tarastrimbulskij@gmail.com>
2025-09-15 13:34:18 +0300

    updade docs

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.fibonacci","l":"Fibonacci"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.fibonacci","l":"Fibonacci"},{"p":"org.fibonacci","l":"Main"}];updateSearchResults();
